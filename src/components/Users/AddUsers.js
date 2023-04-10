b50e3c5b4c79583707fedc559e5838077f1ddb29
vicson <vicsonabuka@gmail.com>
2023-04-10 15:45:07 +0100

    Add managing user the user input state

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
 import './AddUsers.css';
 import Card from "../UI/Card";
+import Button from "../UI/Button";
+
 export default function AddUsers(props){
+const [enteredUsername, setUsername] = useState('');
+const [enteredAge, setAge] = useState('');
+const usernameHandle = (e) => {
+    setUsername(e.target.value);
+}
+const ageHandle = (e) => {
+    setAge(e.target.value);
+}
+    const submitHandle = (e) => {
+    e.preventDefault();
+
+const details = {
+    username: enteredUsername,
+    age: enteredAge
+};
+console.log(details);
+setUsername('');
+setAge('');
+    }
+    
     return(
         <Card className="container">
-         <form>
+         <form onSubmit={submitHandle}>
             <div className="form-control">
         <label>username</label>
-        <input type='text' /> 
+        <input type='text' value={enteredUsername} onChange={usernameHandle} /> 
         <label>Age (years)</label>   
-        <input type='number' min='0'/>
+        <input type='number' value={enteredAge} min='0' onChange={ageHandle}/>
         </div> 
-        <button>Add</button>   
+        <Button type='submit'>Add</Button>
         </form>   
         </Card>
+        
     );
 }
