73e8cbbf1445429317ce5d1cef8f207edac4635c
vicson <vicsonabuka@gmail.com>
2023-04-14 16:39:03 +0100

    Managing the user input state

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -4,22 +4,33 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 export default function AddUsers(props){
+
 const [enteredUsername, setUsername] = useState('');
 const [enteredAge, setAge] = useState('');
+const [isValid, setIsValid] = useState(false);
+
 const usernameHandle = (e) => {
+    setIsValid(false)
     setUsername(e.target.value);
 }
 const ageHandle = (e) => {
+    setIsValid(false)
     setAge(e.target.value);
 }
-    const submitHandle = (e) => {
+const submitHandle = (e) => {
     e.preventDefault();
-
+    if((enteredUsername.trim() && enteredAge.trim()).length === 0) {
+        setIsValid(true);
+        return;
+    }
+   
 const details = {
     username: enteredUsername,
-    age: enteredAge
+    age: enteredAge,
+    id: Math.random().toString()
 };
-console.log(details);
+
+props.addHandle(details);
 setUsername('');
 setAge('');
     }
@@ -28,10 +39,10 @@ setAge('');
         <Card className="container">
          <form onSubmit={submitHandle}>
             <div className="form-control">
-        <label>username</label>
-        <input type='text' value={enteredUsername} onChange={usernameHandle} /> 
-        <label>Age (years)</label>   
-        <input type='number' value={enteredAge} min='0' onChange={ageHandle}/>
+        <label style={{color: isValid ? 'red' : 'black'}}>username</label>
+        <input style={{border: isValid ? '1px solid red' : '1px solid #ccc'}}  type='text' value={enteredUsername} onChange={usernameHandle} /> 
+        <label style={{color: isValid ? 'red' : 'black'}}>Age (years)</label>   
+        <input style={{border: isValid ? '1px solid red' : '1px solid #ccc'}} type='number' value={enteredAge} min='0' onChange={ageHandle}/>
         </div> 
         <Button type='submit'>Add</Button>
         </form>   
