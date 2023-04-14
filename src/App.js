45a49b219d546960dbc6f41793907e86733946d4
vicson <vicsonabuka@gmail.com>
2023-04-14 16:37:04 +0100

    Managging the user input state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React,{useState} from "react";
 import AddUsers from "./components/Users/AddUsers";
+import UserList from "./components/Users/UserList";
+
+
 function App() {
+  const listUser = [
+    
+];
+const [addUser, setAddUser] = useState(listUser);
+
+const onAdding = (lists) => {
+setAddUser(prevUser => {
+  return [lists, ...prevUser]
+});
+}
   return (
     <div>
-      <AddUsers/>
-    </div>
+      <AddUsers addHandle = {onAdding} />
+      <UserList items={addUser}/>
+     </div> 
   );
 }
 
